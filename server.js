52e79c94f9f197b9ebcf29ee68ac9b28f54fb371
BuildTools <arthurpisakhov1@gmail.com>
2019-11-09 20:07:54 -0500

    express-session and config global middleware

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const logger = require('./api/logger');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const userRouter = require('./routes/user-route');
 
+const sessionConfig = {
+  name: 'monkey', // sid
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 30,
+    secure: false, // true in production
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+}
+
 const server = express();
+server.use(session(sessionConfig))
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 
